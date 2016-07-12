8fb86aad7a9e829f41a344c98eae1d72e444d0c2
Nathan Blank <healbadbad@yahoo.com>
2016-07-12 11:53:18 -0500

    abandoning buttons

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var request = require('request');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -29,10 +29,84 @@ app.post('/wow', function(req, res){
   res.status(200);
   res.render('Success!');
 });
+app.get('/test', function(req, res){
+  console.log("doing the one thing");
+  request.post(
+      {url:'https://hooks.slack.com/services/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',
+      // {url:'http://localhost:3000/test',
+      formData:{ payload:
+
+          // '{ "text": "https://slack.com"}'
+
+          '{"bot_access_token":"' + process.env.BOT_API_KEY + '", \
+          \
+          "attachments": [ \
+            {\
+              "title": "The Further Adventures of Slackbot",\
+              "fields": [\
+                {\
+                  "title": "Volume",\
+                  "value": "1",\
+                  "short": true\
+                },\
+                {\
+                  "title": "Issue",\
+                  "value": "3",\
+                  "short": true\
+                }\
+              ],\
+              "author_name": "Stanford S. Strickland",\
+              "author_icon": "https://api.slack.com/img/api/homepage_custom_integrations-2x.png",\
+              "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"\
+            },\
+            {\
+              "title": "Synopsis",\
+              "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."\
+            },\
+            {\
+              "fallback": "Would you recommend it to customers?",\
+              "title": "Would you recommend it to customers?",\
+              "callback_id": "comic_1234_xyz",\
+              "color": "#3AA3E3",\
+              "attachment_type": "default",\
+              "actions": [\
+                {\
+                  "name": "recommend",\
+                  "text": "Recommend",\
+                  "type": "button",\
+                  "value": "recommend"\
+                },\
+                {\
+                  "name": "no",\
+                  "text": "No",\
+                  "type": "button",\
+                  "value": "bad"\
+                }\
+              ]\
+            }\
+          ]}'
+
+
+      }},
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log("success: ", body)
+        } else {
+          console.log("error: ",response.statusCode, body)
+        }
+      }
+  );
+  console.log()
+  res.status(200);
+  res.render('Success!');
+
+});
+app.post('/test', function(req,res){
+  res.status(200);
+  res.render('Post Success!');
+});
 
 // catch 404 and forward to error handler
-
-
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -66,6 +140,8 @@ app.use(function(err, req, res, next) {
 
 
 
+var lunchSessions = []
+var lunchSession = {active:false, users:[]}
 
 var Bot = require('slackbots');
 
@@ -122,15 +198,47 @@ bot.on('start', function() {
       ]
     }
   ]};
-  // bot.postMessageToGroup('random-lunch', 'Hello channel!', params);
-  bot.postMessageToUser('nathanblank', 'hello bro!', params);
+  bot.postMessageToGroup('random-lunch', 'Random Lunch Service Running.');
+  // bot.postMessageToUser('nathanblank', 'hello bro!', params);
   // bot.postMessageToGroup('some-private-group', 'hello group chat!');
 });
 console.log("Posted spam!");
 
 bot.on('message', function(msg){
-  // console.log(msg)
+  if(msg.type == 'message') {
+    text = msg.text;
+    // console.log(text.find("<@U1P11PZLH>"));
+    if(text.startsWith("<@U1P11PZLH>") && (text.split(" ")[1] == "accept" || text.split(" ")[1] == "join")){
+      if(lunchSession.users.indexOf(msg.user) == -1){
+
+        bot.postMessageToGroup('random-lunch', "Lunch invitation accepted. (do stuff here)");
+
+      } else{
+        bot.postMessageToGroup('random-lunch', "Lunch invitation already accepted.");
+      }
+
+      // TODO: send stuff to littlebits session
+    }
+
+    if(text.startsWith("<@U1P11PZLH>") && text.split(" ")[1] == "start"){
+      if(!lunchSession.active){
+        bot.postMessageToGroup('random-lunch', "Test Lunch session started.");
+        lunchSession.active = true;
+        lunchSession.users = [msg.user];
+        var timer = setTimeout(function(){ // 5 minute timeout
+          bot.postMessageToGroup('random-lunch', "Test Lunch session ended with " + lunchSession.users.length + " users");
+          lunchSession.active = false;
+        }, 1000*1*60);
+      } else {
+        bot.postMessageToGroup('random-lunch', "Test Lunch session alread active!. Tell me accept to join the session.");
+      }
+    }
+    // console.log(msg.text);
+
+  }
   return;
 });
+
+
 
 module.exports = app;
